d28673256b8f4a4381e42c648977a36e53e253fd
Rizky Aksyal <rizky.10118212@mahasiswa.unikom.ac.id>
2021-03-29 14:42:43 +0700

    halamn2

--- app/src/main/java/com/example/jadwalku/MainActivity.kt
+++ app/src/main/java/com/example/jadwalku/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.jadwalku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.jadwalku.views.signUp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            val intent = Intent(this, signUp::class.java)
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/jadwalku/views/signUp.kt
+++ app/src/main/java/com/example/jadwalku/views/signUp.kt
@@ -2,10 +2,44 @@ package com.example.jadwalku.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.jadwalku.R
+
 
 class signUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val btnSignup = findViewById<Button>(R.id.btnSignup)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+
+        btnSignup.setOnClickListener {
+            register()
+        }
+
+
+    }
+
+    fun register(){
+        val etNama = findViewById<EditText>(R.id.etNama)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPasword = findViewById<EditText>(R.id.etPassword)
+
+        if(etNama.text.isEmpty()) {
+            etNama.error = "isi Kolom nama !"
+            etNama.requestFocus()
+            return
+        } else if(etEmail.text.isEmpty()) {
+            etEmail.error = "isi Kolom email !"
+            etEmail.requestFocus()
+            return
+        } else if(etPasword.text.isEmpty()) {
+            etPasword.error = "isi Kolom password !"
+            etPasword.requestFocus()
+            return
+        }
     }
 }
